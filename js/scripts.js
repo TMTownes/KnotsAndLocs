9b4bf5e9a9060602d87c539c26a07eb12b06f309
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2023-03-25 14:39:28 -0700

    Create styleList variable with 3 objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello World');
+let styleList = [
+    {name:'Knotless', price: 150, type: 'braids'},
+    {name:'Locs', price: 200, type: 'twists'},
+    {name: 'Feed In', price: 130, type: ['braids','cornrows']}
+];
 
-let favoriteStyle = 'Knotless Braids';
-document.write(favoriteStyle);

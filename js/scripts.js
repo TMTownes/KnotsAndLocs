bfdc96bb1f8dfdf0a08f63b15d698aec7b5ce477
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2023-03-30 14:32:11 -0700

    Added for loop display items

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 let styleList = [
     {name:'Knotless', price: 150, type: 'braids'},
-    {name:'Locs', price: 200, type: 'twists'},
-    {name: 'Feed In', price: 130, type: ['braids','cornrows']}
+    {name:'Locs', price: 245, type: 'twists'},
+    {name: 'Feed In', price: 130, type: ['braids',' cornrows']},
+
+
 ];
 
+let priceAlert = '<p class= "hide">' + 'Wow, that\'s expensive!' + '</p>';
+
+for (i = 0; i<= styleList.length; i++){
+    document.write('<div class= "style__item">' + '<h2>' + styleList[i].name+ '</h2>' + '<h5>' +styleList[i].type + '</h5>' + '<p>' +'Price: $' + styleList[i].price + '</p>' + '</div>');
+    if (styleList[i].price > 200) {
+        document.write(priceAlert);
+    }
+};

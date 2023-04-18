0f8f99aba0f247a839b451979ceb97724ccadf18
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2023-04-17 18:21:52 -0700

    Update forEach/ addListItem to IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,54 @@
-
 //List of Hairstyles
 let styleRepository = (function () {
-  let styleList = [
-    {name:'Knotless', price: 150, type: 'braids'},
-    {name:'Locs', price: 245, type: 'twists'},
-    {name: 'Feed In', price: 130, type: ['braids',' cornrows']}
-  ];  
-    return{
-        //add check for correct formatting
-       add: function (list) {
-            if (typeof "object"){
-                styleList.push(list);
-            }else {
-                console.log("Incorrect data");
-            }
-        },
-        getAll: function() {
-            return styleList;
+    let styleList = [
+      { name: "Knotless", price: 150, type: "braids" },
+      { name: "Locs", price: 245, type: "twists" },
+      { name: "Feed In", price: 130, type: ["braids", " cornrows"] },
+    ];
+    
+      //add check for correct formatting
+      function add(styles) {
+        if (
+          typeof styles === "object" &&
+          "name" in styles &&
+          "price" in styles &&
+          "type" in styles
+          ) {
+          styleList.push(styles);
+        } else {
+          console.log("Incorrect style");
         }
-        //add 'remove' function
+      }
+      //add listItem
+      function addListItem(styles){
+          let styleList = document.querySelector(".style__list");
+          let listItem = document.createElement("li");
+          let button = document.createElement("button");
+          button.innerText = styles.name;
+          button.classList.add("style-button");
+          listItem.appendChild(button);
+          styleList.appendChild(listItem);
+      }
+
+      function getAll() {
+        return styleList;
+      }
+      //add 'remove' function
+      return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem
     };
-})();
-console.log(styleRepository.getAll());
-styleRepository.add({name:'Dreads', price: 1000, type: 'twists'});
-console.log(styleRepository.styleList);
-
-
-
-
-// Obejct.keys(someObject)- displays array of all properties
-
-//Alert for styles over $200
-let priceAlert = '<p class= "hide">' + 'Wow, that\'s expensive!' + '</p>';
-
-
-
-// forEach() function replacing for loop. Prints any array classified as 'list'
-function printArrayDetails(list){
-        document.write('<div class= "style__item">' + '<h2>' + list.name + '</h2>' + '<h5>' +list.type + '</h5>' + '<p>' +'Price: $' + list.price + '</p>' + '</div>');
-    if (list.price > 200){
-        document.write(priceAlert);
-    }
-    };
-
-styleRepository.getAll().forEach(printArrayDetails);
-
-
-
-
+  })();
+  console.log(styleRepository.getAll());
+  styleRepository.add({ name: "Dreads", price: 1000, type: "twists" });
+  console.log(styleRepository.styleList);
+  
+  styleRepository.getAll().forEach(function(styles){
+    styleRepository.addListItem(styles);
+  }); 
+  
+  
+  // Obejct.keys(someObject)- displays array of all properties
+  
+  
